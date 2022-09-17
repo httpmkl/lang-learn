b91224bdef9fe81887bcd766fed3e43415b5678e
rkwan05 <rkwan.hy@gmail.com>
2022-09-17 13:43:26 -0400

    bootstrap, adding posts to sql, modifying html

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
+# import database
+import sqlite3
 from flask import Flask, render_template
+
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    # connection object used to connect with the database
+    return conn
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
